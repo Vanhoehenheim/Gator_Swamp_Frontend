5d3bcd18048304897241a0b3862255352514c999
Prajay P <prajayprashanth@gmail.com>
2025-03-12 19:44:14 -0400

    updated railway api url

--- src/config.jsx
+++ src/config.jsx
@@ -1,10 +1,10 @@
 const config = {
     apiUrl: process.env.NODE_ENV === 'production'
-        ? 'https://gator-swamp-frontend.vercel.app'
+        ? 'gatorswampbackend-production.up.railway.app'
         : 'http://localhost:8080',
     getBaseUrl: () => {
         return process.env.NODE_ENV === 'production'
-            ? 'https://gator-swamp-frontend.vercel.app'
+            ? 'gatorswampbackend-production.up.railway.app'
             : 'http://localhost:8080';
     }
 };
