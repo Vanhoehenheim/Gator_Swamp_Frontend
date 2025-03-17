010cd228973af60a29a61832980d66677e4fb0db
Prajay P <prajayprashanth@gmail.com>
2025-03-16 23:51:32 -0400

    feat: add @vercel/analytics for enhanced analytics tracking

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { LoginForm } from './components/auth/LoginForm';
 import { RegisterForm } from './components/auth/RegisterForm'
 import { Outlet, useNavigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { Analytics } from '@vercel/analytics/next';
 
 // Component to handle app layout
 const AppLayout = () => {
@@ -25,6 +26,7 @@ function App() {
     <ThemeProvider>
       <AuthProvider>
         <div className="bg-stone-100 dark:bg-dark-slate-900 dark:text-gray-200 mx-0 min-h-screen transition-colors duration-200">
+          <Analytics />
           <AppLayout />
         </div>
       </AuthProvider>
